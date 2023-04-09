be1b121ed9cf2291627fd52023511d0967247678
CrioUser <crio-user@criodo.com>
2023-04-09 16:59:59 +0530

    ME_QEATS_V2_MODULE_DEBUG_V2

--- qeatsbackend/src/main/java/com/crio/qeats/controller/RestaurantController.java
+++ qeatsbackend/src/main/java/com/crio/qeats/controller/RestaurantController.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright (c) Crio.Do 2019. All rights reserved
+ * * Copyright (c) Crio.Do 2019. All rights reserved
  *
  */
 
@@ -11,23 +11,29 @@ import com.crio.qeats.exchanges.GetRestaurantsRequest;
 import com.crio.qeats.exchanges.GetRestaurantsResponse;
 import com.crio.qeats.services.RestaurantService;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO: CRIO_TASK_MODULE_RESTAURANTSAPI
+// DONE: CRIO_TASK_MODULE_RESTAURANTSAPI
 // Implement Controller using Spring annotations.
 // Remember, annotations have various "targets". They can be class level, method level or others.
 @Controller
 @RestController
-@RequestMapping(RestaurantController.RESTAURANT_API_ENDPOINT)
 @Log4j2
+@RequestMapping(RestaurantController.RESTAURANT_API_ENDPOINT)
 public class RestaurantController {
 
   public static final String RESTAURANT_API_ENDPOINT = "/qeats/v1";
@@ -39,23 +45,26 @@ public class RestaurantController {
   public static final String POST_ORDER_API = "/order";
   public static final String GET_ORDERS_API = "/orders";
 
+
+
   @Autowired
   private RestaurantService restaurantService;
 
 
-
   @GetMapping(RESTAURANTS_API)
-  public ResponseEntity<GetRestaurantsResponse> getRestaurants( GetRestaurantsRequest getRestaurantsRequest) {
+  public ResponseEntity<GetRestaurantsResponse> getRestaurants(
+      GetRestaurantsRequest getRestaurantsRequest) {
 
-    log.info("getRestaurants called with {}", getRestaurantsRequest);
+// GetRestaurantsRequest newReqObj = new GetRestaurantsRequest(11.7 , 77.9);
+GetRestaurantsRequest newReqObj = getRestaurantsRequest;
+
+    log.info("getRestaurants called with {}", newReqObj);
     GetRestaurantsResponse getRestaurantsResponse;
 
-      //CHECKSTYLE:OFF
-      getRestaurantsResponse = restaurantService
-          .findAllRestaurantsCloseBy(getRestaurantsRequest, LocalTime.now());
-      log.info("getRestaurants returned {}", getRestaurantsResponse);
-      //CHECKSTYLE:ON
-      
+    // CHECKSTYLE:OFF
+    getRestaurantsResponse =
+        restaurantService.findAllRestaurantsCloseBy(newReqObj, LocalTime.now());
+
     if(getRestaurantsResponse != null && !getRestaurantsResponse.getRestaurants().isEmpty()){
       List<Restaurant> restaurants = getRestaurantsResponse.getRestaurants();
       for(int i = 0; i < restaurants.size(); i++){
@@ -63,17 +72,21 @@ public class RestaurantController {
       }
       getRestaurantsResponse.setRestaurants(restaurants);
     }  
+    
 
-      if(getRestaurantsRequest.getLatitude() != null & getRestaurantsRequest.getLongitude() != null
-      && getRestaurantsRequest.getLatitude() >= -90 && getRestaurantsRequest.getLatitude() <= 90 && 
-      getRestaurantsRequest.getLongitude() >= -180 && getRestaurantsRequest.getLongitude() <= 180){
+
+    log.info("getRestaurants returned {}", getRestaurantsResponse);
+    // CHECKSTYLE:ON
+
+
+     if(newReqObj.getLatitude() != null & newReqObj.getLongitude() != null
+      && newReqObj.getLatitude() >= -90 && newReqObj.getLatitude() <= 90 && 
+      newReqObj.getLongitude() >= -180 && newReqObj.getLongitude() <= 180){
         return ResponseEntity.ok().body(getRestaurantsResponse);
       }else{
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
       }
-
-
-    // return ResponseEntity.ok().body(getRestaurantsResponse);
+    
   }
 
   // TIP(MODULE_MENUAPI): Model Implementation for getting menu given a restaurantId.
@@ -87,36 +100,27 @@ public class RestaurantController {
   //
   // HTTP Code: 200
   // {
-  //  "menu": {
-  //    "items": [
-  //      {
-  //        "attributes": [
-  //          "South Indian"
-  //        ],
-  //        "id": "1",
-  //        "imageUrl": "www.google.com",
-  //        "itemId": "10",
-  //        "name": "Idly",
-  //        "price": 45
-  //      }
-  //    ],
-  //    "restaurantId": "11"
-  //  }
+  // "menu": {
+  // "items": [
+  // {
+  // "attributes": [
+  // "South Indian"
+  // ],
+  // "id": "1",
+  // "imageUrl": "www.google.com",
+  // "itemId": "10",
+  // "name": "Idly",
+  // "price": 45
+  // }
+  // ],
+  // "restaurantId": "11"
+  // }
   // }
   // Error Response:
   // HTTP Code: 4xx, if client side error.
-  //          : 5xx, if server side error.
+  // : 5xx, if server side error.
   // Eg:
   // curl -X GET "http://localhost:8081/qeats/v1/menu?restaurantId=11"
-
-
-
-
-
-
-
-
-
 
 
 
